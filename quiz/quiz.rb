43f12bf05495fc361d1fed9a7cec69cbd29680df
AirGear02 <andriydrobot@gmail.com>
2023-05-29 22:36:10 +0300

    Add libraries quiz

---
+++ quiz/quiz.rb
@@ -0,0 +1,17 @@
+require 'singleton'
+
+class Quiz
+  include Singleton
+
+  attr_accessor :yaml_dir, :in_ext, :answers_dir
+
+  def initialize
+    @yaml_dir = "./yml"
+    @in_ext = "*.yml"
+    @answers_dir = ""
+  end
+
+  def config(&block)
+    yield self
+  end
+end
