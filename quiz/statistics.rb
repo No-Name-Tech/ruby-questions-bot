24e8de44482153be63a27af9283540b12184c076
AirGear02 <andriydrobot@gmail.com>
2023-05-29 22:35:57 +0300

    Add statistics

---
+++ quiz/statistics.rb
@@ -0,0 +1,39 @@
+
+
+class Statistics
+  def initialize(writer)
+    @correct_answers = 0
+    @incorrect_answers = 0
+    @writer = writer
+  end
+
+  def correct_answer
+    @correct_answers += 1
+  end
+
+  def incorrect_answer
+    @incorrect_answers += 1
+  end
+
+  def total_answers
+    @correct_answers + @incorrect_answers
+  end
+
+  def correct_percentage
+    correct_percentage = 0
+
+    if total_answers != 0
+      correct_percentage = (@correct_answers.to_f / total_answers.to_f) * 100
+    end
+
+    correct_percentage
+  end
+
+  def report
+    "Всього питань: #{total_answers}\n" + "Правильних відповідей: #{@correct_answers}\n" + "Неправильних відповідей: #{@incorrect_answers}\n" + "Відсоток коректності: #{'%.2f' % correct_percentage}%\n"
+  end
+
+  def print_report
+    @writer.write(report)
+  end
+end
