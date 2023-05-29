048b8c56dc397556a65a6d58aa10bc9f933cc874
Sleentex <56062311+Sleentex@users.noreply.github.com>
2023-05-29 22:45:23 +0300

    Add config

---
+++ quiz/config.rb
@@ -0,0 +1,11 @@
+module Config
+  attr_accessor :yaml_dir, :answers_dir, :in_ext
+
+  def config
+    yield self
+  end
+
+  self.yaml_dir = "quiz/yml"
+  self.answers_dir = "quiz/answers"
+  self.in_ext = "yml"
+end
